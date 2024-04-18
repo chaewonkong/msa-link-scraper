6d02d5b5405188966d79ac77152a58ef2804eff8
chaewonkong <chaewonkong@gmail.com>
2024-04-18 18:38:04 +0900

    refactor: create Meta struct and method

---
+++ scraper/property/constant.go
@@ -0,0 +1,11 @@
+package property
+
+type Type string
+
+const (
+	OpenGraph   Type = "og:"
+	Twitter     Type = "twitter:"
+	Title       Type = "title"
+	Description Type = "description"
+	KeyWords    Type = "keywords"
+)
